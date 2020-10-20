6fc061ae57239c0605e400d70200f0b5b929e07c
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-10-20 23:15:09 +0900

    start

---
+++ src/main/java/com/example/quizapp/QuizAppController.java
@@ -0,0 +1,29 @@
+package com.example.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class QuizAppController {
+    private List<String> quizzes = new ArrayList<>();
+
+    @GetMapping("/show")
+    public List<String> show() {
+        return quizzes;
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestParam String question, @RequestParam boolean answer) {
+        String quiz = question + ":" + answer;
+        /*
+          複数行のコメント
+          コメント
+         */
+        quizzes.add(quiz); // クイズを追加する。
+    }
+}
