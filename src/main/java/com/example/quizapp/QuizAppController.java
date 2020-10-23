eaa969739222774a11523963faa5580a87b05246
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-10-23 21:09:31 +0900

    [start] checkメソッド

--- src/main/java/com/example/quizapp/QuizAppController.java
+++ src/main/java/com/example/quizapp/QuizAppController.java
@@ -10,17 +10,32 @@ import java.util.List;
 
 @RestController
 public class QuizAppController {
+    // クラスQuiz
     private List<Quiz> quizzes = new ArrayList<>();
 
+    // showメソッド
+    // 戻り値 List<Quiz>型
+    // 引数はない
     @GetMapping("/show")
     public List<Quiz> show() {
         return quizzes;
     }
 
+    //createメソッド
+    // 戻り値はない
+    // 引数はString型のquestion,boolean型のAnswer(問題の正解)
     @PostMapping("/create")
     public void create(@RequestParam String question, @RequestParam boolean answer) {
         Quiz quiz = new Quiz(question, answer);
         quizzes.add(quiz);
+    }
+
+    // checkメソッド
+    // 引数はString型のquestion, boolean型のanswer(回答)
+    // 戻り値 正解・不正解かを文字列で返却
+    @GetMapping("/check")
+    public String check(@RequestParam String question,@RequestParam boolean answer){
+        // TODO: 回答が正しいかどうかのチェックして、結果を返却する
     }
 
 }
