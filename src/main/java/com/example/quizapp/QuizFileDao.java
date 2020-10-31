3ecbe69da9f76d65e6cd73cbeb8269298ede7075
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-10-31 12:55:35 +0900

    [fix] quizfiledao

---
+++ src/main/java/com/example/quizapp/QuizFileDao.java
@@ -0,0 +1,23 @@
+package com.example.quizapp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+// Dao ・・・Data access object
+public class QuizFileDao {
+
+    private static final String FILE_PATH = "quizzes.txt";
+
+    public void write(List<Quiz> quizzes) throws IOException {
+        List<String> lines = new ArrayList<>();
+        for (Quiz quiz: quizzes) {
+
+        // 第一引数・・・書き込み先のファイルパス(場所）
+        // 第二引数・・・書き込むデータ List<String>
+        Path path = Paths.get(FILE_PATH);
+        Files.write(path, );
+    }
+}
