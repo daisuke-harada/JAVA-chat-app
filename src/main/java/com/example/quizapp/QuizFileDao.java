47343e49d5ecdff065b0f8b280b821b9c24f32bc
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-11-01 23:50:01 +0900

    [fix] quizzの保存機能

--- src/main/java/com/example/quizapp/QuizFileDao.java
+++ src/main/java/com/example/quizapp/QuizFileDao.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
-// Dao ・・・Data access object
+// Dao ・・・Data access objectの略
 public class QuizFileDao {
 
     private static final String FILE_PATH = "quizzes.txt";
@@ -14,10 +15,13 @@ public class QuizFileDao {
     public void write(List<Quiz> quizzes) throws IOException {
         List<String> lines = new ArrayList<>();
         for (Quiz quiz: quizzes) {
+            lines.add(quiz.toString());
+        }
 
-        // 第一引数・・・書き込み先のファイルパス(場所）
+        // 第一引数・・・書き込み先のファイルパス（場所）
         // 第二引数・・・書き込むデータ List<String>
+
         Path path = Paths.get(FILE_PATH);
-        Files.write(path, );
+        Files.write(path, lines);
     }
 }
